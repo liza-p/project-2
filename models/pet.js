dd25e0e7eea47ff9ff658cee23722e57172356c0
liza p <liyourlp@gmail.com>
2020-07-30 21:15:49 -0700

    updated age data type to a string

--- models/pet.js
+++ models/pet.js
@@ -17,7 +17,7 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false
       },
       age:{
-          type: DataTypes.INTEGER,
+          type: DataTypes.STRING,
           allowNull: false   
       },
       notes: {
