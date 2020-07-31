ef19a0e9c9614f59e0987a32c5064b322d9d6b62
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-07-31 10:27:33 -0700

    email/password

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,4 @@
 USE happy_paw;
 INSERT into users (email, name, password,createdAt,updatedAt)
 VALUES ("liza@gmail.com", "Liza", "password",NOW(),NOW());
+VALUES ("alex@gmail.com", "Alex", "empanadas",NOW(),NOW());
