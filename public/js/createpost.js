bd1b1e4d8a1ad25a5e52f60f18ae0b9691d46de6
Isaias Del Mundo <idelmundo68@gmail.com>
2020-07-30 21:28:04 -0700

    added routes

---
+++ public/js/createpost.js
@@ -0,0 +1,20 @@
+var addCols = function(num) {
+    for (var i = 1; i <= num; i++) {
+        var myCol = $('<div class="col-sm-3 col-md-3 pb-2"></div>');
+        var myPanel = $('<div class="card card-outline-info" id="' + i + 'Panel"><div class="card-block"><div class="card-title"><span>Card #' + i + '</span><button type="button" class="close" data-target="#' + i + 'Panel" data-dismiss="alert"><span class="float-right"><i class="fa fa-remove"></i></span></button></div><p>Some text in ' + i + ' </p><img src="//placehold.it/50/eeeeee" class="rounded rounded-circle"></div></div>');
+        myPanel.appendTo(myCol);
+        myCol.appendTo('#contentPanel');
+    }
+
+
+    $('.close').on('click', function(e) {
+        e.stopPropagation();
+        var $target = $(this).parents('.col-sm-3');
+        $target.hide('slow', function() { $target.remove(); });
+    });
+};
+
+$('#btnGen').click(function() {
+    addCols($('#numPanels').val());
+    return false;
+});
